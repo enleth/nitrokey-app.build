f8fc590fc4b3ff421e78e5f3759712545ab58f23
Szczepan Zalega <szczepan@nitrokey.com>
2017-05-26 20:32:38 +0200

    Add notes for future signing work
    Signed-off-by: Szczepan Zalega <szczepan@nitrokey.com>

--- Windows/codesign.cmd
+++ Windows/codesign.cmd
@@ -2,4 +2,5 @@
 rem %1 file to sign
 rem %2 certificate password
 rem %3 certificate path
+rem You can download signtool from here: https://msdn.microsoft.com/en-us/library/aa387764.aspx
 signtool.exe sign /f %3 /p "%2" /t http://timestamp.comodoca.com/authenticode /fd SHA256 /seal /du https://nitrokey.com /d Nitrokey %1
