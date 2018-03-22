a873dfbe94f8916d2a8b70a66020102f1cc2ce3a
Szczepan Zalega <szczepan@nitrokey.com>
2018-03-22 15:39:13 +0100

    Divide into parts and run in Docker container
    Signed-off-by: Szczepan Zalega <szczepan@nitrokey.com>

--- docker/ubuntu_xenial/install.sh
+++ docker/ubuntu_xenial/install.sh
@@ -1,41 +1,9 @@
+#!/usr/bin/env bash
 # Installs dependencies and builds Nitrokey App's AppImage.
 # Uses /app for making builds and /out for writing results
 # Tested on Ubuntu 16.04, should work on Debian 9 as well
 
-AF="-yq --no-install-suggests --no-install-recommends --force-yes"
-apt-get update -qq
-apt-get install $AF libusb-1.0.0-dev cmake qt5-default qttools5-dev pkg-config libhidapi-dev build-essential git udev fuse qttools5-dev-tools file curl wget
+#bash ./_dependencies.sh
+#bash ./_build.sh
 
-mkdir -p app # in case volume would not be attached to /app
-pushd /app
-wget -c -nv "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
-chmod a+x linuxdeployqt-continuous-x86_64.AppImage
-
-git clone https://github.com/Nitrokey/nitrokey-app --recursive
-
-git pull
-git submodule update --init --recursive
-
-pushd nitrokey-app
-
-source /etc/os-release
-OS=$ID.$VERSION_ID
-SUFFIX=$OS-`date +"%Y%m%d.%H%M%S"`
-BUILDDIR=build-appimage-$SUFFIX
-APPDIR=$BUILDDIR/nitrokey-app.AppImage/usr
-OUTDIR=/out/$SUFFIX
-
-mkdir $BUILDDIR -p
-pushd $BUILDDIR
-qmake .. PREFIX=$APPDIR
-make -j4 install
-popd
-
-../linuxdeployqt-continuous-x86_64.AppImage $APPDIR/share/applications/nitrokey-app.desktop -bundle-non-qt-libs
-../linuxdeployqt-continuous-x86_64.AppImage $APPDIR/share/applications/nitrokey-app.desktop -appimage
-
-mkdir -p $OUTDIR
-mv -v Nitrokey_App-x86_64.AppImage $OUTDIR/
-
-
-popd
+docker build . -t nkapp:appimage
