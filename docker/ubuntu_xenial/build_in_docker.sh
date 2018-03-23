df5d20133b1d6087e70dfbab52538339e568c996
Szczepan Zalega <szczepan@nitrokey.com>
2018-03-23 11:06:09 +0100

    Add git version to AppImage name. Pass addtional parameters to run and build script.
    Signed-off-by: Szczepan Zalega <szczepan@nitrokey.com>

--- docker/ubuntu_xenial/build_in_docker.sh
+++ docker/ubuntu_xenial/build_in_docker.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 mkdir -p out
-docker run --rm -it --privileged -v `readlink -f out`:/out nkapp:appimage
+docker run --rm -it --privileged -v `readlink -f out`:/out nkapp:appimage $@
+
