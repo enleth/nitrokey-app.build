28c9a8ed86996eeef1065d2f3dd34abf56a0c5c9
Szczepan Zalega <szczepan@nitrokey.com>
2018-03-30 17:19:46 +0200

    Moving AppImage building to Ubuntu 14.04 Trusty
    Signed-off-by: Szczepan Zalega <szczepan@nitrokey.com>

--- docker/ubuntu_xenial/_dependencies.sh
+++ docker/ubuntu_xenial/_dependencies.sh
@@ -2,11 +2,18 @@
 
 AF="-yq --no-install-suggests --no-install-recommends --force-yes"
 apt-get update -qq
-apt-get install $AF ca-certificates libusb-1.0.0-dev cmake qt5-default qttools5-dev libqt5svg5-dev pkg-config libhidapi-dev build-essential git udev fuse qttools5-dev-tools file curl wget
+apt-get install software-properties-common
+add-apt-repository -y ppa:ubuntu-toolchain-r/test
+add-apt-repository --yes ppa:beineri/opt-qt594-trusty
+apt-get -qq update -qq
+apt-get -qq install $AF ca-certificates libusb-1.0.0-dev cmake pkg-config libhidapi-dev git udev fuse file curl wget
+apt-get -qq install ${AF} g++-5
+apt-get -qq install ${AF} qt59base qt59tools qt59svg
+apt-get -qq remove gcc-4.8 g++-4.8 --purge
+
 
 pushd /
 wget -c -nv "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
 chmod a+x linuxdeployqt-continuous-x86_64.AppImage
-
 popd
 
